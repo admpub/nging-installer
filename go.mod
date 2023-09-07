47a6f88c31ae1095ecd810b7964f8104a12ce132
Hank Shen <swh@admpub.com>
2023-09-07 19:36:53 +0800

    update

--- go.mod
+++ go.mod
@@ -3,20 +3,23 @@ module github.com/admpub/nging-installer
 go 1.21.1
 
 require (
-	github.com/admpub/confl v0.2.4
+	github.com/admpub/go-download/v2 v2.1.13
 	github.com/webx-top/com v0.9.6
 )
 
 require (
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
 	github.com/admpub/fsnotify v1.5.0 // indirect
-	github.com/araddon/gou v0.0.0-20211019181548-e7d08105776c // indirect
 	github.com/francoispqt/gojay v1.2.13 // indirect
-	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/goccy/go-json v0.7.9 // indirect
 	github.com/hashicorp/go-version v1.6.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/vbauerster/mpb/v6 v6.0.4 // indirect
+	golang.org/x/sys v0.0.0-20210930212924-f542c8878de8 // indirect
+	golang.org/x/text v0.3.1-0.20180807135948-17ff2d5776d2 // indirect
 )
--- main.go
+++ main.go
@@ -1,487 +1,189 @@
+// nging-installer [install|upgrade|uninstall] 5.1.0
+
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"regexp"
 	"runtime"
 	"strings"
-	"time"
 
-	"github.com/admpub/confl"
+	godl "github.com/admpub/go-download/v2"
+	"github.com/admpub/go-download/v2/progressbar"
 	"github.com/webx-top/com"
 )
 
-// usage:
-// 1. go run main.go min
-// 2. go run main.go linux_arm64 min
-
-var p = buildParam{}
-
-var c = Config{
-	GoVersion:    `1.20.6`,
-	Executor:     `nging`,
-	NgingVersion: `5.1.1`,
-	NgingLabel:   `stable`,
-	Project:      `github.com/admpub/nging`,
-	VendorMiscDirs: map[string][]string{
-		`*`: {
-			`vendor/github.com/nging-plugins/caddymanager/template/`,
-			`vendor/github.com/nging-plugins/collector/template/`,
-			`vendor/github.com/nging-plugins/dbmanager/template/`,
-			`vendor/github.com/nging-plugins/dbmanager/public/assets/`,
-			`vendor/github.com/nging-plugins/ddnsmanager/template/`,
-			`vendor/github.com/nging-plugins/dlmanager/template/`,
-			`vendor/github.com/nging-plugins/frpmanager/template/`,
-			`vendor/github.com/nging-plugins/ftpmanager/template/`,
-			`vendor/github.com/nging-plugins/servermanager/template/`,
-			`vendor/github.com/nging-plugins/sshmanager/template/`,
-			`vendor/github.com/nging-plugins/webauthn/template/`,
-		},
-		`linux`: {
-			`vendor/github.com/nging-plugins/firewallmanager/template/`,
-		},
-		`!linux`: {},
-	},
-	BuildTags: []string{`bindata`, `sqlite`},
-	CopyFiles: []string{`config/ua.txt`, `config/config.yaml.sample`, `data/ip2region`, `config/preupgrade.*`},
-	MakeDirs:  []string{`public/upload`, `config/vhosts`, `data/logs`},
-	Compiler:  `xgo`,
+var osName = runtime.GOOS
+var archName = runtime.GOARCH
+var operate = `install`
+var version = `5.1.2`
+var saveDir = `nging`
+var softwareURL = `https://img.nging.coscms.com/nging/v%s/`
+var binName = "nging"
+var fileName = fmt.Sprintf("nging_%s_%s", osName, archName)
+var fileFullName = fileName + ".tar.gz"
+var softwareFullURL string
+var workDir string
+var local string
+var supports = map[string][]string{
+	`darwin`:  {`amd64`, `arm64`},
+	`linux`:   {`386`, `amd64`, `arm64`, `arm-7`, `arm-6`, `arm-5`},
+	`windows`: {`386`, `amd64`},
 }
 
-var targetNames = map[string]string{
-	`linux_386`:     `linux/386`,
-	`linux_amd64`:   `linux/amd64`,
-	`linux_arm5`:    `linux/arm-5`,
-	`linux_arm6`:    `linux/arm-6`,
-	`linux_arm7`:    `linux/arm-7`,
-	`linux_arm64`:   `linux/arm64`,
-	`darwin_amd64`:  `darwin/amd64`,
-	`darwin_arm64`:  `darwin/arm64`,
-	`windows_386`:   `windows/386`,
-	`windows_amd64`: `windows/amd64`,
+func parseArgs() {
+	flag.StringVar(&local, `local`, ``, `--local ./nging_darwin_amd64.tar.gz`)
+	flag.StringVar(&softwareURL, `softwareURL`, softwareURL, `--softwareURL `+softwareURL)
+	flag.Parse()
+	if len(os.Args) > 4 {
+		os.Args = os.Args[0:4]
+	}
+	switch len(os.Args) {
+	case 4:
+		saveDir = os.Args[3]
+		fallthrough
+	case 3:
+		version = os.Args[2]
+		version = strings.TrimPrefix(version, `v`)
+		fallthrough
+	case 2:
+		operate = os.Args[1]
+	}
 }
 
-var armRegexp = regexp.MustCompile(`/arm`)
-var configFile = `./builder.conf`
+func verifyOSAndArch() {
+	if _, ok := supports[osName]; !ok {
+		com.ExitOnFailure(`Unsupported System:`+osName, 1)
+	}
+	switch archName {
+	case `x86_64`:
+		archName = "amd64"
+	case "i386", "i686":
+		archName = "386"
+	case "aarch64_be", "aarch64", "armv8b", "armv8l", "armv8", "arm64":
+		archName = "arm64"
+	case "armv7":
+		archName = "arm-7"
+	case "armv7l":
+		archName = "arm-6"
+	case "armv6":
+		archName = "arm-6"
+	case "armv5":
+		archName = "arm-5"
+	case "arm":
+		armVersion := os.Getenv(`GOARM`)
+		switch armVersion {
+		case `7`, `6`, `5`:
+			archName = "arm-" + armVersion
+		default:
+			archName = "arm-5"
+		}
+	}
+	if !com.InSlice(archName, supports[osName]) {
+		com.ExitOnFailure(`Unsupported Arch:`+archName, 1)
+	}
+}
 
 func main() {
-	flag.StringVar(&configFile, `conf`, configFile, `--conf `+configFile)
-	flag.Parse()
-
-	_, err := confl.DecodeFile(configFile, &c)
+	verifyOSAndArch()
+	parseArgs()
+	if len(local) > 0 {
+		fmt.Println(`local: `, local)
+	}
+	if !strings.HasPrefix(softwareURL, `/`) {
+		softwareURL += `/`
+	}
+	softwareFullURL = fmt.Sprintf(softwareURL, version) + fileFullName
+	var err error
+	workDir, err = filepath.Abs(saveDir)
 	if err != nil {
 		com.ExitOnFailure(err.Error(), 1)
 	}
-	c.apply()
-	p.ProjectPath, err = com.GetSrcPath(p.Project)
-	if err != nil {
-		com.ExitOnFailure(err.Error(), 1)
-	}
-	p.WorkDir = strings.TrimSuffix(strings.TrimSuffix(p.ProjectPath, `/`), p.Project)
-	var targets []string
-	var armTargets []string
-	addTarget := func(target string, notNames ...bool) {
-		if len(notNames) == 0 || !notNames[0] {
-			target = getTarget(target)
-			if len(target) == 0 {
-				return
-			}
-		}
-		if armRegexp.MatchString(target) {
-			armTargets = append(armTargets, target)
-		} else {
-			targets = append(targets, target)
-		}
-	}
-	var minify bool
-	switch len(os.Args) {
-	case 3:
-		minify = isMinified(os.Args[2])
-		addTarget(os.Args[1])
-	case 2:
-		switch {
-		case isMinified(os.Args[1]):
-			minify = true
-			for _, t := range targetNames {
-				addTarget(t, true)
-			}
-		case os.Args[1] == `genConfig`:
-			b, err := confl.Marshal(c)
-			if err != nil {
-				com.ExitOnFailure(err.Error(), 1)
-			}
-			err = os.WriteFile(configFile, b, os.ModePerm)
-			if err != nil {
-				com.ExitOnFailure(err.Error(), 1)
-			}
-			com.ExitOnSuccess(`successully generate config file: ` + configFile)
-			return
-		case os.Args[1] == `makeGen`:
-			makeGenerateCommandComment(c)
-			return
-		default:
-			addTarget(os.Args[1])
-		}
-	case 1:
-		for _, t := range targetNames {
-			addTarget(t, true)
-		}
+	switch operate {
+	case `un`, `uninstall`:
+		uninstall()
+	case `up`, `upgrade`:
+		upgrade()
+	case `install`:
+		install()
 	default:
-		com.ExitOnFailure(`参数错误`)
+		install()
 	}
-	fmt.Println(`WorkDir: `, p.WorkDir)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	err = os.Chdir(p.ProjectPath)
-	if err != nil {
-		com.ExitOnFailure(err.Error(), 1)
-	}
-	p.NgingCommitID = execGitCommitIDCommand(ctx)
-	p.NgingBuildTime = time.Now().Format(`20060102150405`)
-	if minify {
-		p.MinifyFlags = []string{`-s`, `-w`}
-	}
-	distPath := filepath.Join(p.ProjectPath, `dist`)
-	fmt.Println(`DistPath: `, distPath)
-	allTargets := append(targets, armTargets...)
-	fmt.Printf("Building %s for %+v\n", p.Executor, allTargets)
-	for _, target := range allTargets {
-		parts := strings.SplitN(target, `/`, 2)
-		if len(parts) != 2 {
-			continue
-		}
-		pCopy := p
-		pCopy.Target = target
-		pCopy.PureGoTags = []string{`osusergo`}
-		osName := parts[0]
-		archName := parts[1]
-		pCopy.ReleaseDir = filepath.Join(distPath, p.Executor+`_`+osName+`_`+archName)
-		pCopy.goos = osName
-		pCopy.goarch = archName
-		if osName != `darwin` {
-			pCopy.LdFlags = []string{`-extldflags`, `'-static'`}
-		}
-		if osName != `windows` {
-			pCopy.PureGoTags = append(pCopy.PureGoTags, `netgo`)
-		} else {
-			pCopy.Extension = `.exe`
-		}
-		execGenerateCommand(ctx, pCopy)
-		err := com.MkdirAll(pCopy.ReleaseDir, os.ModePerm)
+}
+
+func downloadAndExtract() {
+	compressedFile := fileFullName
+	if len(local) == 0 {
+		godlOpt := &godl.Options{}
+		progress := progressbar.New(godlOpt, 50)
+		defer progress.Wait()
+
+		_, err := godl.Download(softwareFullURL, compressedFile, godlOpt)
 		if err != nil {
 			com.ExitOnFailure(err.Error(), 1)
-		}
-		execBuildCommand(ctx, pCopy)
-		packFiles(pCopy)
-	}
-}
-
-func getTarget(target string) string {
-	if t, y := targetNames[target]; y {
-		return t
-	}
-	for _, t := range targetNames {
-		if t == target {
-			return t
-		}
-	}
-	return ``
-}
-
-func isMinified(arg string) bool {
-	return arg == `m` || arg == `min`
-}
-
-type buildParam struct {
-	Config
-	Target         string //${GOOS}/${GOARCH}
-	ReleaseDir     string
-	Extension      string
-	PureGoTags     []string
-	NgingBuildTime string
-	NgingCommitID  string
-	MinifyFlags    []string
-	LdFlags        []string
-	ProjectPath    string
-	WorkDir        string
-	goos           string
-	goarch         string
-}
-
-func (p buildParam) genLdFlagsString() string {
-	ldflags := []string{}
-	ldflags = append(ldflags, p.MinifyFlags...)
-	ldflags = append(ldflags, p.LdFlags...)
-	return `-X main.BUILD_TIME=` + p.NgingBuildTime + ` -X main.COMMIT=` + p.NgingCommitID + ` -X main.VERSION=` + p.NgingVersion + ` -X main.LABEL=` + p.NgingLabel + ` -X main.BUILD_OS=` + runtime.GOOS + ` -X main.BUILD_ARCH=` + runtime.GOARCH + ` ` + strings.Join(ldflags, ` `)
-}
-
-func (p buildParam) genEnvVars() []string {
-	env := []string{`GOOS=` + p.goos}
-	parts := strings.SplitN(p.goarch, `-`, 2)
-	if parts[0] == `arm` {
-		env = append(env, `GOARCH=`+parts[0])
-		if len(parts) == 2 {
-			env = append(env, `GOARM=`+parts[1])
 		}
 	} else {
-		env = append(env, `GOARCH=`+p.goarch)
+		compressedFile = local
 	}
-	return env
+	err := com.MkdirAll(saveDir, os.ModePerm)
+	if err != nil {
+		com.ExitOnFailure(err.Error(), 1)
+	}
+	_, err = com.UnTarGz(compressedFile, saveDir)
+	if err != nil {
+		com.ExitOnFailure(err.Error(), 1)
+	}
+	distDir := filepath.Join(saveDir, fileName)
+	err = com.CopyDir(distDir, saveDir)
+	if err != nil {
+		com.ExitOnFailure(err.Error(), 1)
+	}
+	os.RemoveAll(distDir)
+	if len(local) == 0 {
+		os.Remove(compressedFile)
+	}
+	os.Chmod(filepath.Join(saveDir, binName), os.ModePerm)
 }
 
-func execBuildCommand(ctx context.Context, p buildParam) {
-	tags := []string{}
-	tags = append(tags, p.PureGoTags...)
-	tags = append(tags, p.BuildTags...)
-	var args []string
-	var env []string
-	var workDir string
-	var compiler string
-	switch p.Compiler {
-	case `go`:
-		workDir = filepath.Join(p.WorkDir, p.Project)
-		compiler = p.Compiler
-		com.MkdirAll(p.ReleaseDir, os.ModePerm)
-		args = []string{`build`,
-			`-tags`, strings.Join(tags, ` `),
-			`-ldflags`, p.genLdFlagsString(),
-			`-o`, filepath.Join(p.ReleaseDir, p.Executor+`-`+p.goos+`-`+p.goarch),
-		}
-		env = append(env, os.Environ()...)
-		env = append(env, p.genEnvVars()...)
-		if p.CgoEnabled {
-			env = append(env, `CGO_ENABLED=1`)
-		} else {
-			env = append(env, `CGO_ENABLED=0`)
-		}
-	case `xgo`:
-		fallthrough
-	default:
-		workDir = p.WorkDir
-		compiler = `xgo`
-		image := p.GoImage
-		if len(image) == 0 {
-			image = `localhost/crazymax/xgo:` + p.GoVersion
-		} else {
-			checkStr := image
-			pos := strings.LastIndex(image, `/`)
-			if pos > -1 {
-				checkStr = image[pos:]
-			}
-			if !strings.Contains(checkStr, `:`) {
-				image += `:` + p.GoVersion
-			}
-		}
-		args = []string{
-			`-go`, p.GoVersion,
-			`-goproxy`, `https://goproxy.cn,direct`,
-			`-image`, image,
-			`-targets`, p.Target,
-			`-dest`, p.ReleaseDir,
-			`-out`, p.Executor,
-			`-tags`, strings.Join(tags, ` `),
-			`-ldflags`, p.genLdFlagsString(),
-			`./` + p.Project,
-		}
-	}
-	cmd := exec.CommandContext(ctx, compiler, args...)
+func execServiceCommand(op string, mustSucceed ...bool) error {
+	cmd := exec.Command(`./`+binName, `service`, op)
 	cmd.Dir = workDir
-	cmd.Stdin = os.Stdin
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	cmd.Env = env
-	err := cmd.Run()
-	if err != nil {
-		com.ExitOnFailure(err.Error(), 1)
-	}
-}
-
-func execGenerateCommand(ctx context.Context, p buildParam) {
-	cmd := exec.CommandContext(ctx, `go`, `generate`)
-	cmd.Dir = p.ProjectPath
-	cmd.Stdin = os.Stdin
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	cmd.Env = append(cmd.Env, os.Environ()...)
-	cmd.Env = append(cmd.Env, p.genEnvVars()...)
-	err := cmd.Run()
-	if err != nil {
-		com.ExitOnFailure(err.Error(), 1)
-	}
-}
-
-func execGitCommitIDCommand(ctx context.Context) string {
-	cmd := exec.CommandContext(ctx, `git`, `rev-parse`, `HEAD`)
-	cmd.Dir = p.ProjectPath
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		com.ExitOnFailure(err.Error(), 1)
-	}
-	return string(out)
-}
-
-func packFiles(p buildParam) {
-	files, err := filepath.Glob(filepath.Join(p.ReleaseDir, p.Executor+`-`+p.goos+`*`))
-	if err != nil {
-		com.ExitOnFailure(err.Error(), 1)
-	}
-	for _, file := range files {
-		com.Rename(file, filepath.Join(p.ReleaseDir, p.Executor+p.Extension))
-		break
-	}
-	for _, copyFile := range p.CopyFiles {
-		f := filepath.Join(p.ProjectPath, copyFile)
-		if strings.Contains(f, `*`) {
-			files, err = filepath.Glob(f)
-			if err != nil {
-				com.ExitOnFailure(err.Error(), 1)
-			}
-			for _, file := range files {
-				destFile := filepath.Join(p.ReleaseDir, strings.TrimPrefix(file, p.ProjectPath))
-				com.MkdirAll(filepath.Dir(destFile), os.ModePerm)
-				err = com.Copy(file, destFile)
-				if err != nil {
-					com.ExitOnFailure(err.Error(), 1)
-				}
-			}
-			continue
-		}
-		if com.IsDir(f) {
-			err = com.CopyDir(f, filepath.Join(p.ReleaseDir, copyFile))
-			if err != nil {
-				com.ExitOnFailure(err.Error(), 1)
-			}
-			continue
-		}
-		destFile := filepath.Join(p.ReleaseDir, copyFile)
-		com.MkdirAll(filepath.Dir(destFile), os.ModePerm)
-		err = com.Copy(f, destFile)
-		if err != nil {
+		if len(mustSucceed) == 0 || mustSucceed[0] {
 			com.ExitOnFailure(err.Error(), 1)
 		}
+		return err
 	}
-	for _, newDir := range p.MakeDirs {
-		err = com.MkdirAll(filepath.Join(p.ReleaseDir, newDir), os.ModePerm)
-		if err != nil {
-			com.ExitOnFailure(err.Error(), 1)
-		}
-	}
-	err = com.TarGz(p.ReleaseDir, p.ReleaseDir+`.tar.gz`)
+	fmt.Println(string(out))
+	return err
+}
+
+func install() {
+	downloadAndExtract()
+	execServiceCommand(`install`)
+	execServiceCommand(`start`)
+	fmt.Println(`🎉 Congratulations! Installed successfully.`)
+}
+
+func uninstall() {
+	execServiceCommand(`stop`)
+	execServiceCommand(`uninstall`)
+	fmt.Println(`🎉 Congratulations! Successfully uninstalled.`)
+	err := os.RemoveAll(saveDir)
 	if err != nil {
 		com.ExitOnFailure(err.Error(), 1)
 	}
-	err = os.RemoveAll(p.ReleaseDir)
-	if err != nil {
-		com.ExitOnFailure(err.Error(), 1)
-	}
-	// 解压: tar -zxvf nging_linux_amd64.tar.gz -C ./nging_linux_amd64
+	fmt.Println(`🎉 Congratulations! File deleted successfully.`)
 }
 
-func genComment(vendorMiscDirs ...string) string {
-	comment := "//go:generate go install github.com/admpub/bindata/v3/go-bindata@latest\n"
-	comment += `//go:generate go-bindata -fs -o bindata_assetfs.go -ignore "\\.(git|svn|DS_Store|less|scss|gitkeep)$" -minify "\\.(js|css)$" -tags bindata`
-	prefixes := []string{}
-	miscDirs := []string{`public/assets/`, `template/`, `config/i18n/`}
-	miscDirs = append(miscDirs, vendorMiscDirs...)
-	uniquePrefixes := map[string]struct{}{}
-	for k, v := range miscDirs {
-		if !strings.HasSuffix(v, `/...`) {
-			if !strings.HasSuffix(v, `/`) {
-				v += `/`
-			}
-			v += `...`
-		}
-		if strings.HasPrefix(v, `vendor/`) {
-			parts := strings.SplitN(v, `/`, 5)
-			if len(parts) == 5 {
-				prefix := strings.Join(parts[0:4], `/`) + `/`
-				if _, ok := uniquePrefixes[prefix]; !ok {
-					uniquePrefixes[prefix] = struct{}{}
-					prefixes = append(prefixes, prefix)
-				}
-			}
-		}
-		miscDirs[k] = v
-	}
-	comment += ` -prefix "` + strings.Join(prefixes, `|`) + `" `
-	comment += strings.Join(miscDirs, ` `)
-	return comment
-}
-
-func makeGenerateCommandComment(c Config) {
-	dfts := c.VendorMiscDirs[`*`]
-	for osName, miscDirs := range c.VendorMiscDirs {
-		if osName == `*` {
-			continue
-		}
-		dirs := make([]string, 0, len(dfts)+len(miscDirs))
-		dirs = append(dirs, dfts...)
-		dirs = append(dirs, miscDirs...)
-		fileName := `main_`
-		if strings.HasPrefix(osName, `!`) {
-			fileName += `non` + strings.TrimPrefix(osName, `!`)
-		} else {
-			fileName += osName
-		}
-		fileName += `.go`
-		filePath := filepath.Join(p.ProjectPath, fileName)
-		fileContent := "//go:build " + osName + "\n\n"
-		fileContent += "package main\n\n"
-		fileContent += genComment(dirs...) + "\n\n"
-		b, err := os.ReadFile(filePath)
-		if err == nil {
-			old := string(b)
-			pos := strings.Index(old, `import `)
-			if pos > -1 {
-				fileContent += old[pos:]
-			}
-		}
-		err = os.WriteFile(filePath, []byte(fileContent), os.ModePerm)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-	}
-}
-
-type Config struct {
-	GoVersion      string
-	GoImage        string
-	Executor       string
-	NgingVersion   string
-	NgingLabel     string
-	Project        string
-	VendorMiscDirs map[string][]string // key: GOOS
-	BuildTags      []string
-	CopyFiles      []string
-	MakeDirs       []string
-	Compiler       string
-	CgoEnabled     bool
-}
-
-func (a Config) apply() {
-	if len(a.GoVersion) > 0 {
-		p.GoVersion = a.GoVersion
-	}
-	if len(a.Executor) > 0 {
-		p.Executor = a.Executor
-	}
-	if len(a.NgingVersion) > 0 {
-		p.NgingVersion = a.NgingVersion
-	}
-	if len(a.NgingLabel) > 0 {
-		p.NgingLabel = a.NgingLabel
-	}
-	if len(a.Project) > 0 {
-		p.Project = a.Project
-	}
-	p.BuildTags = a.BuildTags
-	p.CopyFiles = a.CopyFiles
-	p.MakeDirs = a.MakeDirs
-	p.Compiler = a.Compiler
-	p.CgoEnabled = a.CgoEnabled
+func upgrade() {
+	execServiceCommand(`stop`)
+	execServiceCommand(`stop`, false)
+	downloadAndExtract()
+	execServiceCommand(`start`)
+	fmt.Println(`🎉 Congratulations! Successfully upgraded.`)
 }
